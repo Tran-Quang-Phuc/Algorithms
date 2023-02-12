ea5080e679c58b0e67f3ff2dd6e0943ab530fd4b
Tran-Quang-Phuc <phuc.tq204679@sis.hust.edu.vn>
2023-02-13 00:07:43 +0700

    3/4 test

---
+++ Thực hành thuật toán ứng dụng/Buổi 1/RMQ.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define N 1000000
+
+int A[N], n, ans;
+int M[N][30];
+
+void input()
+{
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &A[i]);
+}
+
+void process()
+{
+    for(int i = 0; i < n; i++)
+        M[i][0] = i;
+    
+    for(int j = 1; 1 << j <= n; j++)
+    {
+        for(int i = 0; i + (1 << j) - 1 < n; i++)
+        {
+            if(A[M[i][j-1]] <= A[M[i + (1 << (j-1))][j-1]])
+                M[i][j] = M[i][j-1];
+            else
+                M[i][j] = M[i + (1 << (j-1))][j-1];
+        }
+    }
+}
+
+int RMQ(int i, int j)
+{
+    int k = (int)log2(j-i+1);
+    return min(A[M[i][k]], A[M[j - (1 << k) + 1][k]]);
+}
+
+int main() {
+    input();
+    process();
+    int m, i, j;
+    scanf("%d", &m);
+    ans = 0;
+    for(int k = 0; k < m; k++)
+    {
+        scanf("%d%d", &i, &j);
+        // cout << RMQ(i, j) << endl;
+        ans = ans + RMQ(i, j);
+    }
+    printf("%d", ans);
+    return 0;
+}
